37df1504b1e0228a8d6d61ccff5183345f2867a4
Mizba <mizbafathimathul@gmail.com>
2025-06-20 11:01:07 +0530

    created demo

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>this is a sample demo</h1>
+    </div>
+  );
+}
